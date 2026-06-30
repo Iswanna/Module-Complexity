c99312813b9a1f69b547cc9470ec4f64d386e8eb
iswat <ibeetek@gmail.com>
2026-06-30 13:00:54 +0100

    feat: add LinkedList class and initialize head and tail pointers
    - Define the LinkedList class in linked_list.py.
    - Implement the __init__ method to track the head and tail of the list.
    - Initialize head and tail to None for an empty list state.

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -7,3 +7,12 @@ class Node:
         self.value = value
         self.next = None      # The person ahead
         self.previous = None  # The person behind
+
+
+class LinkedList:
+    """
+    Manages the line by keeping track of the Head and the Tail.
+    """
+    def __init__(self):
+        self.head = None
+        self.tail = None

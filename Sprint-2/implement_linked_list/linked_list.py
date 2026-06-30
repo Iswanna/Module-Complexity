47ed0155276a969860a56ee0cf858154a9703f07
iswat <ibeetek@gmail.com>
2026-06-30 13:06:16 +0100

    feat: implement push_head method in LinkedList
    - Added the logic to create a new node and insert it at the front of the list.
    
    - Handled pointer updates for both empty and non-empty list states.
    
    - Returns the new node to allow for future removal/referencing.

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -16,3 +16,22 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def push_head(self, value):
+        """Adds a new node to the front of the list."""
+        new_node = Node(value)
+
+        # Logic 2: If the list is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        # Logic 3: If the list is not empty
+        else:
+            new_node.next = self.head      # New person grabs old head's hand
+            self.head.previous = new_node  # Old head reaches back to new person
+            self.head = new_node           # New person is now the head
+        
+        # Logic 4: Return the node so we can reference it later
+        return new_node
+
+    
